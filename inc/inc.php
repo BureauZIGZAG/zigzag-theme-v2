0de7d792713db5fe0ce67ee6701551ba2bc91d9c
Freekiehsoes <freekattema@live.nl>
2022-11-30 23:01:19 +0100

    yes

--- inc/inc.php
+++ inc/inc.php
@@ -3,6 +3,7 @@
 namespace Theme\Inc;
 
 require_once __DIR__ . '/ThemeBuffer.php';
+require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/autoload.php';
 
 require_once __DIR__ . '/inc.vite.php';
