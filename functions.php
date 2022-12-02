b0af3e3caa4d5ee0b456bf3833038e07b0299ee6
Freekiehsoes <freekattema@live.nl>
2022-12-02 17:55:57 +0100

    THEME UPDATES

--- functions.php
+++ functions.php
@@ -6,20 +6,20 @@ if ( ! defined( 'ABSPATH' ) )
 
 include_once "inc/inc.php";
 
-\Freekattema\Wp\ThemeSetup\Theme::get()
-    ->HideAdminPosts()
-    ->HideAdminComments()
-    ->HideAdminTools()
-    ->removeDefaultWordpressStyling()
-    ->adminBarCleanup()
-    ->removeWordpressJquery()
-    ->hideMustUsePlugins()
-    ->hideAdminDashboard()
-    ->hideAdminBar()
-    ->hideAdminDashboardWidgets()
-    ->registerNavMenus([
-        'primary' => 'Primary Menu',
-        'footer' => 'Footer Menu',
-    ]);
-
+\Freekattema\Wp\ThemeSetup\Theme::init([
+    'hide_admin_posts' => true,
+    'hide_admin_comments' => true,
+    'hide_admin_tools' => true,
+    'remove_default_wordpress_styling' => true,
+    'remove_wordpress_jquery' => true,
+    'hide_must_use_plugins' => true,
+    'hide_admin_dashboard' => true,
+    'hide_admin_bar' => true,
+    'admin_bar_cleanup' => true,
+    'hide_admin_dashboard_widgets' => true,
+    'register_nav_menus' => [
+        'main-menu' => 'Main Menu',
+        'footer-menu' => 'Footer Menu',
+    ],
+]);
 
