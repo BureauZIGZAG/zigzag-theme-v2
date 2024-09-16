59a794d52c8f9a52b187ddeb5c57fe7bdc2663da
Harmen Gerritsen <harmen@bureauzigzag.nl>
2024-09-16 17:02:23 +0200

    improved front-page.php

--- front-page.php
+++ front-page.php
@@ -1,8 +1,11 @@
 <?php // Template name: Home
 get_header(); ?>
-<main>
-    <h1>
-        test
-    </h1>
-</main>
+    <main>
+        <?php $title = get_the_title() ?: 'Test' ?>
+        <h1>
+            <?php echo $title ?>
+        </h1>
+
+        <?php ContentComponent::display(); ?>
+    </main>
 <?php get_footer(); ?>
